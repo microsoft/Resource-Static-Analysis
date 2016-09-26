be4774b9b2ea3e76b5059b0b6821e4a4e29ec013
Michael Chen <mchen@microsoft.com>
2016-09-26 16:29:53 -0700

    Fixing a issue which cause rule loading failed when the engine is using pre-parsed COs

--- ResourceStaticAnalysis/src/UnitTests/TestHelpers.cs
+++ ResourceStaticAnalysis/src/UnitTests/TestHelpers.cs
@@ -6,8 +6,10 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.ResourceStaticAnalysis.UnitTests.TestObjectStructure;
+using Microsoft.ResourceStaticAnalysis.Core.DataSource;
 using Microsoft.ResourceStaticAnalysis.Core.DataSource.DataSourcePackages;
 using Microsoft.ResourceStaticAnalysis.Core.Engine;
 using Microsoft.ResourceStaticAnalysis.Core.Output;
@@ -27,7 +29,7 @@ namespace Microsoft.ResourceStaticAnalysis.UnitTests
         internal static EngineConfig CreateSampleConfig(TestContext testContext, string testRuleFileName, int numberOfResources)
         {
             testRuleFileName = Path.Combine(testContext.DeploymentDirectory, testRuleFileName);
-            
+
             var configuration = new EngineConfig();
 
             #region DataSourceProviderTypes
@@ -98,6 +100,16 @@ namespace Microsoft.ResourceStaticAnalysis.UnitTests
         }
 
         /// <summary>
+        /// Generate a List of <see cref="ClassificationObject"/>.
+        /// </summary>
+        /// <param name="numberOfResources">Number of Resources to generate (in increments of 6)</param>
+        /// <returns>A list of <see cref="ClassificationObject"/>.</returns>
+        internal static List<ClassificationObject> GenerateParesdCOs(int numberOfResources)
+        {
+            return ParseResStrInfoToCOs(GenerateLargeNumberOfResources(numberOfResources)).Cast<ClassificationObject>().ToList();
+        }
+
+        /// <summary>
         /// Generates a List of Tuple to be converted to ResourceEntries during execution (minimum of 6)
         /// </summary>
         /// <param name="numResources"></param>
@@ -109,17 +121,40 @@ namespace Microsoft.ResourceStaticAnalysis.UnitTests
                 throw new Exception("Method generates a minimum of 6 Tuples");
             }
             List<Tuple<string, string, string>> results = new List<Tuple<string, string, string>>();
-            for (int i = 1; i <= numResources; i+=6)
+            for (int i = 1; i <= numResources; i += 6)
             {
                 results.Add(new Tuple<string, string, string>("Item" + i.ToString(), "foo", "descriptive comment"));
-                results.Add(new Tuple<string, string, string>("Item" + ( i + 1).ToString(), "foo", "foo"));
-                results.Add(new Tuple<string, string, string>("Itm" + (i + 2).ToString(), "foo", "foo"));
+                results.Add(new Tuple<string, string, string>("Item" + (i + 1).ToString(), "foo", "foo"));
+                results.Add(new Tuple<string, string, string>("Item" + (i + 2).ToString(), "foo", "foo"));
                 results.Add(new Tuple<string, string, string>("Item" + (i + 3).ToString(), "foobar", "foobar"));
                 results.Add(new Tuple<string, string, string>("Item" + (i + 4).ToString(), "foobar", "foo"));
                 results.Add(new Tuple<string, string, string>("Item" + (i + 5).ToString(), "xyz", "no vowels"));
             }
 
             return results;
+        }
+
+        /// <summary>
+        /// This method is used to parse a list of tuple to a list of <see cref="SampleClassificationObjectType"/>.
+        /// </summary>
+        /// <param name="sampleResources">A list of tuple to be parsed.</param>
+        /// <returns>A list of <see cref="SampleClassificationObjectType"/>.</returns>
+        private static List<SampleClassificationObjectType> ParseResStrInfoToCOs(List<Tuple<string, string, string>> resourceTuples)
+        {
+            SampleResourceCollection sampleResourceCollection = new SampleResourceCollection(resourceTuples);
+
+            List<SampleClassificationObjectType> sampleClassificationObjects = new List<SampleClassificationObjectType>();
+
+            SamplePropertyAdapter samplePropertyAdapter = new SamplePropertyAdapter();
+            sampleResourceCollection.Entries.ForEach(sampleResource =>
+            {
+                PropertyProvider propertyProvider = new PropertyProvider();
+                propertyProvider.AddPropertyAdapterDataSourcePair(samplePropertyAdapter, sampleResource);
+                SampleClassificationObjectType sampleClassificationObject = new SampleClassificationObjectType(propertyProvider);
+                sampleClassificationObjects.Add(sampleClassificationObject);
+            });
+
+            return sampleClassificationObjects;
         }
     }
 }
